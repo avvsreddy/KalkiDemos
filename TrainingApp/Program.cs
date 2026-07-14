9bfdcf1a5cc7376f1f057842cfc416bfb13d9bcd
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-07-14 15:32:39 +0530

    training app code

---
+++ TrainingApp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace TrainingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Orgranization
+    {
+        public string Name { get; set; }
+    }
+    class Trainer
+    {
+        //private Orgranization org = new Orgranization();
+        public Orgranization Org { get; set; }
+
+        public List<Trainee> Trainees { get; set; } = new List<Trainee>();
+
+        public List<Training> Trainings { get; set; } = new List<Training>();
+    }
+    class Trainee
+    {
+        public Trainer TheTrainer { get; set; }
+        public List<Training> Trainings { get; set; } = new List<Training>();
+
+    }
+    class Training
+    {
+        public Trainer TheTrainer { get; set; }
+        public List<Trainee> Trainees { get; set; } = new List<Trainee>();
+        public Course TheCourse { get; set; }
+    }
+    class Course
+    {
+        public List<Training> Trainings { get; set; } = new List<Training>();
+        public List<Module> Modules { get; set; } = new List<Module>();
+    }
+    class Module
+    {
+        public List<Unit> Units { get; set; } = new List<Unit>();
+    }
+    class Unit
+    {
+        public int DurationHrs { get; set; }
+        public List<Topic> Topics { get; set; } = new List<Topic>();
+    }
+    class Topic
+    {
+
+    }
+}
