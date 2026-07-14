7573f4eeaca4c8cc2076ce8c0d842dd7f00625d0
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-07-14 17:06:41 +0530

    ide code

---
+++ IDEApp/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace IDEApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpLanguage cs = new CSharpLanguage();
+            JavaLanguage java = new JavaLanguage();
+            CLanguage c = new CLanguage();
+            IDE ide = new IDE();
+            ide.Languages.Add(cs);
+            //ide.Languages.Add(java);
+            ide.Languages.Add(c);
+            //ide.CS = cs;
+            //ide.Java = java;
+            //ide.C = c;
+            ide.DoWork();
+
+
+        }
+    }
+
+    class IDE // OCP
+    {
+        //public CSharpLanguage CS { get; set; }
+        //public JavaLanguage Java { get; set; }
+        //public CLanguage C { get; set; }
+
+        public List<ILanguage> Languages { get; set; } = new List<ILanguage>();
+
+
+        public void DoWork() 
+        {
+            foreach (var language in Languages)
+            {
+                Console.WriteLine(language.GetName());
+                Console.WriteLine(language.GetUnit());
+                Console.WriteLine(language.GetParadigm());
+                Console.WriteLine("----------------------");
+            }
+
+            //Console.WriteLine(Java.GetName());
+            //Console.WriteLine(Java.GetUnit());
+            //Console.WriteLine(Java.GetParadigm());
+            //Console.WriteLine("----------------------");
+            //Console.WriteLine(C.GetName());
+            //Console.WriteLine(C.GetUnit());
+            //Console.WriteLine(C.GetParadigm());
+            //Console.WriteLine("----------------------");
+
+        }
+    }
+
+
+    public interface ILanguage
+    {
+        string GetName();
+        string GetUnit();
+        string GetParadigm();
+    }
+
+
+    class CSharpLanguage : ILanguage 
+    {
+        public string GetName()
+        {
+            return "C# Language";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetParadigm()
+        {
+            return "Object-Oriented";
+        }
+    }
+
+    class JavaLanguage : ILanguage
+    {
+        public string GetName()
+        {
+            return "Java Language";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetParadigm()
+        {
+            return "Object-Oriented";
+        }
+    }
+
+    class CLanguage : ILanguage
+    {
+        public string GetName()
+        {
+            return "C Language";
+        }
+        public string GetUnit()
+        {
+            return "Function";
+        }
+        public string GetParadigm()
+        {
+            return "Procedure-Oriented";
+        }
+    }
+}
