765792fd2d61ea9abbc69a087aa8db2e20fc4246
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2026-07-16 11:12:39 +0530

    api1

---
+++ KalkiTechProductsCatalogService/Controllers/ProductsCatalogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KalkiTechProductsCatalogService.Controllers
+{
+
+    //GET https://www.kt.com/api/ProductsCatalog
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsCatalogController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetData() // endpoint
+        {
+            return "Hello from ProductsCatalogController!";
+        }
+    }
+}
